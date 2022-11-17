f7cbfa4be02f8c635bd8a058c21c73f138dc018f
Y <Yannic_Studer1@sluz.ch>
2022-11-17 17:34:03 +0100

    WPF_FirstApp

--- 3. jahr/M120/WpfApp1/MainWindow.xaml.cs
+++ 3. jahr/M120/WpfApp1/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WpfApp1
+namespace WPF_FirstApp
 {
     /// <summary>
     /// Interaktionslogik für MainWindow.xaml
